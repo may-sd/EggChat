2bbbc811c185055c72aa031ad784fc71d28c6b6a
may-s-d <may.s.dong@gmail.com>
2023-12-24 18:37:02 -0500

    Refactor transpilers into separate methods

--- EggChat/patches/HUDManagerPatch.cs
+++ EggChat/patches/HUDManagerPatch.cs
@@ -7,9 +7,20 @@ namespace EggChat.Patches;
 [HarmonyPatch(typeof(HUDManager))]
 internal class HUDManagerPatch {
     [HarmonyPatch("SubmitChat_performed")]
-    [HarmonyPatch("AddPlayerChatMessageServerRpc")]
     [HarmonyTranspiler]
     private static IEnumerable<CodeInstruction> SubmitChat_performed_transpiler(IEnumerable<CodeInstruction> instructions) {
+        foreach (var instruction in instructions) {
+            if (instruction.opcode == OpCodes.Ldc_I4_S && (sbyte)instruction.operand == 50) {
+                yield return new CodeInstruction(OpCodes.Ldc_I4_S, 127);
+            } else {
+                yield return instruction;
+            }
+        }
+    }
+
+    [HarmonyPatch("AddPlayerChatMessageServerRpc")]
+    [HarmonyTranspiler]
+    private static IEnumerable<CodeInstruction> AddPlayerChatMessageServerRpc_transpiler(IEnumerable<CodeInstruction> instructions) {
         foreach (var instruction in instructions) {
             if (instruction.opcode == OpCodes.Ldc_I4_S && (sbyte)instruction.operand == 50) {
                 yield return new CodeInstruction(OpCodes.Ldc_I4_S, 127);
